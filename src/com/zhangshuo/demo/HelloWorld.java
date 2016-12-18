30928c4cb2e859f7b6cfebc69fc415182e3e5026
Shuo Zhang <zhangshuogit@qq.com>
2016-12-18 18:54:17 +0800

    Initial commit

---
+++ src/com/zhangshuo/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zhangshuo.demo;
+
+/**
+ * Created by zhangshuo on 16-12-18.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
